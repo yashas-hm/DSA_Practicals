18d1b2c275a8c7a9cefb0496764bb514f4bff4bf
yashas-hm <yashashm@gmail.com>
2021-04-18 22:32:08 +0530

    all done

---
+++ LAB2.6_1.c
@@ -0,0 +1,72 @@
+// C program for different tree traversals
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int data){
+    struct Node* node
+        = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+
+    return (node);
+}
+
+void printPostorder(struct Node* node){
+    if (node == NULL)
+        return;
+
+    printPostorder(node->left);
+
+    printPostorder(node->right);
+
+    printf("%d ", node->data);
+}
+
+void printInorder(struct Node* node){
+    if (node == NULL)
+        return;
+
+    printInorder(node->left);
+
+    printf("%d ", node->data);
+
+    printInorder(node->right);
+}
+
+void printPreorder(struct Node* node){
+    if (node == NULL)
+        return;
+
+    printf("%d ", node->data);
+
+    printPreorder(node->left);
+
+    printPreorder(node->right);
+}
+
+int main(){
+    struct Node* root = newNode(1);
+
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    printf("\nPreorder traversal of binary tree is \n");
+    printPreorder(root);
+
+    printf("\nInorder traversal of binary tree is \n");
+    printInorder(root);
+
+    printf("\nPostorder traversal of binary tree is \n");
+    printPostorder(root);
+
+    return 0;
+}
